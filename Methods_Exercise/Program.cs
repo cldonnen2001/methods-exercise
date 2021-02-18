29cde901aecd8c4f7cd310fe83f4822affe2f6e6
Christine Godfrey <cldonnen2001@yahoo.com>
2021-02-17 19:54:55 -0600

    method exercise

---
+++ Methods_Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Methods_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Hi my name is {name}.  My pet is a {color} {animal}.  He likes to listen to {band}");
+        }
+        {
+
+        }
+        
+    }
+}
